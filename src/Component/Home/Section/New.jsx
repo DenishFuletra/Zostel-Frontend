106a8e5f41030036cf1b54ce0916b8a0dc5167eb
Denish Fuletra <fuletradenish@gmail.com>
2023-02-07 00:31:09 +0530

    added backend

--- src/Component/Home/Section/New.jsx
+++ src/Component/Home/Section/New.jsx
@@ -10,7 +10,7 @@ function New() {
     let [data, setData] = useState([]);
 
     let fetchData = async () => {
-        let responce = await fetch(`https://resisted-cubic-zydeco.glitch.me/new`);
+        let responce = await fetch(`http://localhost:3000/news`);
         let data = await responce.json();
         // console.log(data);
         setData(data);
